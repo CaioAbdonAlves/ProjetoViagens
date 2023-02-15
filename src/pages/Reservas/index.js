a513c5b1d6e0f424ffcde0850da1fd21097e216e
CaioAbdonA <caio.alves54@etec.sp.gov.br>
2023-02-15 02:14:56 -0300

    criação da lista de reservas dinâmica
    Acesso ao state global com o hook useSelector da lib react-redux para montar a lista de reservas solicitadas pelo usuário

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,19 +1,23 @@
+import { useSelector } from 'react-redux';
 import './style.css';
 import { MdDelete } from 'react-icons/md';
 
 export default function Reservas() {
+    const reserves = useSelector(state => state.rootReducer.reserve);
     return(
       <div>
-        <h1 className='title'>Você solicitou 1 reservas</h1>
+        <h1 className='title'>Você solicitou {reserves.length} reservas</h1>
 
-        <div className="reservas">
-          <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="Maceio" />
-          <strong>Viagem Maceio 7 Dias</strong>
-          <span>Quantidade: 2</span>
-          <button type='button' onClick={() => {}}>
-            <MdDelete size={20} color="#191919"/>
-          </button>
-        </div>
+        {reserves.map((reserve) => (
+          <div key={reserve.id} className="reservas">
+            <img src={reserve.image} alt={reserve.title} />
+            <strong>{reserve.title}</strong>
+            <span>Quantidade: 2</span>
+            <button type='button' onClick={() => {}}>
+              <MdDelete size={20} color="#191919"/>
+            </button>
+          </div>
+        ))}
 
         <footer>
           <button type='button'>Solicitar Reservas</button>
